73c832fed1d7ecd662bfc0cbdf4d2602fd1d7a9e
Youness El Andaloussi <youness@elandaloussi.net>
2026-07-01 00:53:41 +0100

    committed examples

--- crates/vybe_compiler/examples/dump_fortran_ast.rs
+++
@@ -1,9 +0,0 @@
-use std::env;
-use std::fs;
-
-fn main() {
-    let path = env::args().nth(1).expect("expected source path");
-    let source = fs::read_to_string(&path).expect("read source");
-    let module = vybe_compiler::languages::fortran::parse(&source).expect("parse fortran");
-    println!("{:#?}", module);
-}
--- crates/vybe_compiler/examples/go_ast.rs
+++
@@ -1,5 +0,0 @@
-fn main() {
-    let src = "package main; import \"fmt\"; func main() { fmt.Println(0xFF); }";
-    let ast = vybe_compiler::languages::go::parse(src).unwrap();
-    println!("{:#?}", ast);
-}
